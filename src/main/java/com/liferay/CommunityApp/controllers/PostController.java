47c5b44555fc19511b51a5247e8d79f9711f85ea
Lucas Davi <lucasdavi99@hotmail.com>
2023-10-07 17:30:57 -0300

    Alterando a funcionalidade de postagem
    adicionado a clase de serviço da entidade Community e alterado a função de postagem

--- src/main/java/com/liferay/CommunityApp/controllers/PostController.java
+++ src/main/java/com/liferay/CommunityApp/controllers/PostController.java
@@ -1,28 +1,57 @@
 package com.liferay.CommunityApp.controllers;
 
 import com.liferay.CommunityApp.dtos.PostDTO;
+import com.liferay.CommunityApp.models.CommunityModel;
 import com.liferay.CommunityApp.models.PostModel;
+import com.liferay.CommunityApp.models.UserModel;
+import com.liferay.CommunityApp.repositories.CommunityRepository;
+import com.liferay.CommunityApp.service.CommunityService;
 import com.liferay.CommunityApp.service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class PostController {
 
     @Autowired
     PostService postService;
+    @Autowired
+    CommunityService communityService;
 
     @PostMapping("/new-post")
-    public ResponseEntity<PostModel> savePost(@RequestBody @Valid PostDTO postDTO) {
+    public ResponseEntity<PostModel> savePost(@RequestBody @Valid PostDTO postDTO, @RequestParam("communityId") UUID communityId) {
 
-        var postModel = new PostModel();
-        BeanUtils.copyProperties(postDTO, postModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(postService.createPost(postModel));
+        // Obtém o usuário autenticado
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        // Obtém a comunidade a partir do ID da comunidade
+        CommunityModel community = communityService.findById(communityId);
+
+        // Define o autor e a comunidade da postagem
+        UserModel currentUser = (UserModel) userDetails;
+        PostModel post = new PostModel();
+        post.setAuthor(currentUser);
+        post.setCommunity(community);
+
+        // Copia as propriedades do DTO para a entidade PostModel
+        BeanUtils.copyProperties(postDTO, post);
+
+        // Crie a postagem
+        PostModel createdPost = postService.createPost(post);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 }
---
+++ src/main/java/com/liferay/CommunityApp/service/CommunityService.java
@@ -0,0 +1,13 @@
+package com.liferay.CommunityApp.service;
+
+import com.liferay.CommunityApp.models.CommunityModel;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CommunityService {
+    public CommunityModel findById(UUID communityId) {
+        return null;
+    }
+}

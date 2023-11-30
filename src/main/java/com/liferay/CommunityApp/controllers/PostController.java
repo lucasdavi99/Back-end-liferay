60d44e51b60d84f4fb163af76c0e342d80e3eb02
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-11-30 14:37:58 -0300

    Documentando a API

--- src/main/java/com/liferay/CommunityApp/controllers/PostController.java
+++ src/main/java/com/liferay/CommunityApp/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.liferay.CommunityApp.exceptions.CommunityException;
 import com.liferay.CommunityApp.exceptions.CustomAuthenticationException;
 import com.liferay.CommunityApp.models.PostModel;
 import com.liferay.CommunityApp.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ public class PostController {
     @Autowired
     PostService postService;
 
+    @Operation(summary = "Cria um novo post em uma comunidade específica.")
     @PostMapping("/new-post/{communityName}")
     public ResponseEntity<Object> create(@RequestBody @Valid PostDTO postDTO, @PathVariable(value = "communityName") String communityName) {
         try {
@@ -39,6 +41,7 @@ public class PostController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza um post existente pelo seu ID.")
     public ResponseEntity<Object> update(@PathVariable("id") UUID id, @RequestBody @Valid PostDTO postDTO) {
         try {
             PostModel postModel = new PostModel();
@@ -56,6 +59,7 @@ public class PostController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta um post pelo seu ID.")
     public ResponseEntity<Object> delete(@PathVariable("id") UUID id) {
         try{
             postService.deletePost(id);
@@ -66,6 +70,7 @@ public class PostController {
     }
 
     @GetMapping
+    @Operation(summary = "Lista todos os posts")
     public ResponseEntity<List<PostModel>> findAll() {
         List<PostModel> list = postService.findAll();
         return ResponseEntity.ok().body(list);

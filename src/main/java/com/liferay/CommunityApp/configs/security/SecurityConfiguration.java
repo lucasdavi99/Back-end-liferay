32c8c8f0a80a705c199d97cad79b0805a07871d4
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-12-14 14:19:10 -0300

    Modificando segurança deixando mais robusto

--- src/main/java/com/liferay/CommunityApp/configs/security/SecurityConfiguration.java
+++ src/main/java/com/liferay/CommunityApp/configs/security/SecurityConfiguration.java
@@ -31,22 +31,6 @@ public class SecurityConfiguration {
     @Autowired
     SecurityFilter securityFilter;
 
-    private static final String[] SWAGGER_URL = {
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/webjars/**",
-            "/swagger-ui/**",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
-            "/swagger-ui.html/**",
-            "/swagger-docs/**"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
 
@@ -58,9 +42,13 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(Arrays.toString(SWAGGER_URL))).permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST, "/auth/register")).permitAll()
-                        .requestMatchers(mvcMatcherBuilder.pattern(HttpMethod.POST, "/posts/new-post/{communityName}")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/users/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/posts/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/communities/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/comments/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/invite/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/dm/**")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/reports/**")).hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)

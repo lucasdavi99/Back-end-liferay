5069511ddddc53e772bc2d411f71a81dad87741f
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-12-17 22:58:10 -0300

    Conclusão da feature de resetar senha

--- src/main/java/com/liferay/CommunityApp/configs/password/PasswordResetTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.liferay.CommunityApp.configs.password;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    PasswordResetToken findByToken(String passwordResetToken);
-}
